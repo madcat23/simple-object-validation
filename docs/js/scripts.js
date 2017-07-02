53cb5cf2a8d0be1397838cd548ef3f0b77f8dae1
madcat23 <madcat23@kaffeeschluerfer.com>
2017-07-02 13:22:48 +0200

    Updated docs

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -87,6 +87,9 @@ $(document).ready(function() {
     })
   }
 
+  $('pre').wrap('<div class="pre-wrapper"></div>')
+  $('.pre-wrapper').append('<span class="pre-info-text"><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp; Press CTRL and click on a function name to jump to its definition.</span>')
+
   $('pre a').on('click', function(event) {
     event.preventDefault()
     if ($('body').hasClass('control-key-down')) {
